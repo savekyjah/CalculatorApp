0328ed77ee2ffde8aa63b44083f3ebd736d9f0ba
zsavery <zsavery998@gmail.com>
2024-02-03 14:18:42 -0600

    Notes on Data Types
    Co-Authored-By: savekyjah <158771811+savekyjah@users.noreply.github.com>

--- Albert/src/main/java/org/example/Main.java
+++ Albert/src/main/java/org/example/Main.java
@@ -3,15 +3,31 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    // public static int temp = 34;
+    // private  static int roomTemp = 72;
+    public static void main(String[] args) {
+        // System.out.println("The temp outside is " + temp + ".");
+        // System.out.println("The temp inside is " + roomTemp + ".");
+
+        //Data Types
+//        int a = 10;
+//        long b = 20;
+//        float c = 67.89F;
+//        double d = 56.334;
+//        boolean flag = true; //0 or 1
+//        char intitial = 'Z';
+
+
+
+
+
+
+        //User input and String Manipulation
+
+        //Operations
+
+        //Function
+
     }
 }
