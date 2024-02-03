668f87ba8d2e76bb90c00b3c23b033d2fd50bfd0
zsavery <zsavery998@gmail.com>
2024-02-03 15:05:10 -0600

    Add Addition, Subtraction, and Exit funtionallity
    Co-Authored-By: savekyjah <158771811+savekyjah@users.noreply.github.com>

--- Albert/src/main/java/org/example/Main.java
+++ Albert/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -18,16 +20,56 @@ public class Main {
 //        boolean flag = true; //0 or 1
 //        char intitial = 'Z';
 
-
-
-
-
-
         //User input and String Manipulation
+        boolean flag = true;
+        while(flag) {
+            System.out.println("Enter selection choice: ");
+            System.out.println("Addition");
+            System.out.println("Subtraction");
+            System.out.println("Exit");
+            System.out.print("-> ");
 
-        //Operations
+            // Create Scanner object
+            Scanner scan = new Scanner(System.in);
+            //Get user input
+            String selection = scan.next();
+            // System.out.println(selection);
 
-        //Function
+            //Get num1 and num2 from user input
+            double num1 = 0, num2 = 0;
+            if(!selection.equals("Exit")) {
+                System.out.print("\nEnter number 1: ");
+                num1 = scan.nextDouble();
 
+                System.out.print("\nEnter number 2: ");
+                num2 = scan.nextDouble();
+            }
+            //a = 5 , b = 5, c = 8
+            // a == b, c > b, a < c & b < c
+            double result;
+            if (selection.equals("Addition")) {
+                result = Add(num1, num2);
+                System.out.println(result);
+            } else if (selection.equals("Subtraction")) {
+                result = Subtract(num1, num2);
+                System.out.println(result);
+            } else if(selection.equals("Exit")){
+                flag = false;
+            } else {
+                System.out.println("Try again. Enter 'Exit' to quit.");
+            }
+
+
+        }
+    }
+    // Functions
+    public static double Add(double a, double b){
+        return a + b;
+    }
+    public static double Subtract(double a, double b){
+        if(b > a){
+            return b - a;
+        }
+        return a - b;
     }
 }
